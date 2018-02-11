c1072e7fbfb0a70f2415a9876924e64be05dbce8
Sparsh <skkumarsparsh@gmail.com>
2018-02-11 19:07:00 +0530

    Panel Colors changing properly

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -12,6 +12,9 @@ export class FormComponent implements OnInit {
 
   data;
   step = -1;
+  initial;
+  previousPanelControl;
+  previousPanel;
 
   Feedbacks = [
     {value: '0', viewValue: 'Excellent'},
@@ -22,14 +25,12 @@ export class FormComponent implements OnInit {
     {value: '5', viewValue: 'Very Bad'},
   ]
 
-  model = new Form(1, '', '', 'http://');
-
   teacherControl1 = new Array(12);
-  teacherControl2 = new FormControl('', [Validators.required]);
-  teacherControl3 = new FormControl('', [Validators.required]);
-  teacherControl4 = new FormControl('', [Validators.required]);
-  teacherControl5 = new FormControl('', [Validators.required]);
-  teacherControl6 = new FormControl('', [Validators.required]);
+  teacherControl2 = new Array(12);
+  teacherControl3 = new Array(12);
+  teacherControl4 = new Array(12);
+  teacherControl5 = new Array(12);
+  teacherControl6 = new Array(12);
 
   constructor(private http:Http) {
     for(var i=0;i<12;i++) {
@@ -40,43 +41,57 @@ export class FormComponent implements OnInit {
       this.teacherControl5[i] = new FormControl('', [Validators.required]);
       this.teacherControl6[i] = new FormControl('', [Validators.required]);
     }
+    this.initial=0;
   }
 
   ngOnInit() {
   }
 
-  get currentField() { 
-    return JSON.stringify(this.model);
-  }
-
   getData() {
-    let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded'});
-    let options = new RequestOptions({ headers: headers });
-    this.http.post('http://127.0.0.1:8080', this.model, options).subscribe(res => {
-      this.data = res.json() || {};
-      console.log(this.data);
-    });
+    // let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded'});
+    // let options = new RequestOptions({ headers: headers });
+    // this.http.post('http://127.0.0.1:8080', this.model, options).subscribe(res => {
+    //   this.data = res.json() || {};
+    //   console.log(this.data);
+    // });
   }
 
-  setStep(index: number) {
+  setStep(index: number,panelcontrol,panel) {
     this.step = index;
+    if(this.initial!=0) {
+      this.changeColor(this.previousPanelControl,this.previousPanel);
+      this.previousPanelControl = panelcontrol;
+      this.previousPanel = panel;
+    } else {
+      this.previousPanelControl = panelcontrol;
+      this.previousPanel = panel;
+      this.initial=1;
+    }
   }
 
-  nextStep() {
+  nextStep(panelcontrol,panel) {
     this.step++;
-    // if(this.feedbackControl.hasError('required')) {
-    //   console.log("Input 1 error");
-    // }
-    // if(this.feedbackControl1.hasError('required')) {
-    //   console.log("Input 2 error");
-    // }
-    // if(this.feedbackControl2.hasError('required')) {
-    //   console.log("Input 3 error");
-    // }
+    this.changeColor(panelcontrol,panel);
   }
 
-  prevStep() {
+  prevStep(panelcontrol,panel) {
     this.step--;
+    this.changeColor(panelcontrol,panel);
+  }
+
+  changeColor(panelcontrol,panel) {
+    var j=0;
+    for(var i=0;i<12;i++) {
+      if(panelcontrol[i].hasError('required')) {
+        j=1;
+        break;
+      }
+    }
+    if(j==1) {
+      document.getElementById(panel).setAttribute("style","background-color:rgba(255,0,0,0.2);");
+    } else {
+      document.getElementById(panel).setAttribute("style","background-color:rgba(0,255,0,0.2);");
+    }
   }
 
 }
