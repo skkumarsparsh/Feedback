7c4ee6753a4903fafceac09e791b34cde8ccdac8
Sparsh <skkumarsparsh@gmail.com>
2018-02-11 18:02:59 +0530

    Feedback questions complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -23,6 +23,7 @@ import { FormComponent } from './form/form.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     BrowserAnimationsModule,
     MatCardModule,
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Form } from '../form.model';
 import { Http,Headers,RequestOptions,RequestMethod } from '@angular/http';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -18,12 +19,28 @@ export class FormComponent implements OnInit {
     {value: '2', viewValue: 'Good'},
     {value: '3', viewValue: 'Satisfactory'},
     {value: '4', viewValue: 'Bad'},
-    {value: '1', viewValue: 'Very Bad'},
+    {value: '5', viewValue: 'Very Bad'},
   ]
 
   model = new Form(1, '', '', 'http://');
 
-  constructor(private http:Http) { }
+  teacherControl1 = new Array(12);
+  teacherControl2 = new FormControl('', [Validators.required]);
+  teacherControl3 = new FormControl('', [Validators.required]);
+  teacherControl4 = new FormControl('', [Validators.required]);
+  teacherControl5 = new FormControl('', [Validators.required]);
+  teacherControl6 = new FormControl('', [Validators.required]);
+
+  constructor(private http:Http) {
+    for(var i=0;i<12;i++) {
+      this.teacherControl1[i] = new FormControl('', [Validators.required]);
+      this.teacherControl2[i] = new FormControl('', [Validators.required]);
+      this.teacherControl3[i] = new FormControl('', [Validators.required]);
+      this.teacherControl4[i] = new FormControl('', [Validators.required]);
+      this.teacherControl5[i] = new FormControl('', [Validators.required]);
+      this.teacherControl6[i] = new FormControl('', [Validators.required]);
+    }
+  }
 
   ngOnInit() {
   }
@@ -47,6 +64,15 @@ export class FormComponent implements OnInit {
 
   nextStep() {
     this.step++;
+    // if(this.feedbackControl.hasError('required')) {
+    //   console.log("Input 1 error");
+    // }
+    // if(this.feedbackControl1.hasError('required')) {
+    //   console.log("Input 2 error");
+    // }
+    // if(this.feedbackControl2.hasError('required')) {
+    //   console.log("Input 3 error");
+    // }
   }
 
   prevStep() {
